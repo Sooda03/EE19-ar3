2f70ffcac3db6c3ff720c51327b5f2c0da2022db
Pontus pålsson <pontus.palsson@elev.ga.ntig.se>
2021-11-22 08:56:32 +0100

    mixat

---
+++ Gymnasiearbete/loggain.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="image">
+    </div>
+    <div class="kontainer">
+        <h1>Logga in!</h1>
+        <p>Användarnamn</p>
+        <input type="username">
+        <p>Lösenord</p>
+        <input type="password">
+        <button>Logga in!</button>
+        <a href="./registrera.html"> >>Registrera dig hos oss!<< </a>
+    </div>
+</body>
+</html>
